1fa62d4d95aba1b290d0d33efff6b9d1cc9d0afb
Autumn Wilmoth <austin.wilmoth895@gmail.com>
2026-05-10 18:21:54 -0700

    added mobile view, new images, more content and made the images scale correctly

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
-fetch('/components/header.html')
+let HEADER_TYPE = 'header.html';
+
+if (window.innerWidth <= 768) {
+    HEADER_TYPE = 'mobileHeader.html';
+}
+
+let HEADER_PATH = `../components/${HEADER_TYPE}`;
+let FOOTER_PATH = '../components/footer.html';
+
+console.log('Current URL:', window.location.hostname);
+if (window.location.href.hostname === 'awilmoth895.github.io') {
+    HEADER_PATH = 'website/components/header.html';
+    FOOTER_PATH = 'website/components/footer.html';    
+}
+
+
+
+fetch(HEADER_PATH)
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-container').innerHTML = data;
     setActiveLink();
 });
 
-fetch('../components/footer.html')
+fetch(FOOTER_PATH)
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
